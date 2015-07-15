528a7d3233730f32ef4b3f32f9b03f28b304c842
Ryan Seys <ryanseys@google.com>
2015-07-15 14:55:37 +0900

    Initial commit

---
+++ src/main/java/com/ryanseys/HelloWorld.java
@@ -0,0 +1,24 @@
+package com.ryanseys;
+
+/**
+ * A simple Hello World class for learning about Android dependencies.
+ */
+public class HelloWorld {
+
+    private String message;
+
+    // Base constructor.
+    public void HelloWorld() {
+        this.message = "Hello World!";
+    }
+
+    // Get the message.
+    public String getMessage() {
+        return message;
+    }
+
+    // Set the message.
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
